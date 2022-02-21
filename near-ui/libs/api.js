22a0131c336e91bc97575674bdacb552599a88f9
lanbery <lanbery@gmail.com>
2022-02-21 18:14:05 +0800

    upd: add NEAR Aurora Network support beta

--- near-ui/libs/api.js
+++ near-ui/libs/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASEURL = "http://192.168.81.170:3000";
+const API_BASEURL = "http://drops.aibyb.com.cn/";
 
 export const get = async (path, params) => {
   const res = await axios.get(API_BASEURL + path, {
